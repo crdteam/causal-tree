7113003c64bd538cbb5e2f060bb9b1879a4777d6
ArturAssisComp <arturassiscomp@gmail.com>
2023-08-03 22:38:20 -0300

    Implement unit test cases for the String method of InsertChar

--- src/causal_tree/operation_insert_char_test.go
+++ src/causal_tree/operation_insert_char_test.go
@@ -13,3 +13,45 @@ func Test_AtomPriority(t *testing.T) {
 	})
 
 }
+
+func Test_String(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    InsertChar
+		expected string
+	}{
+		{
+			name:     "Empty rune",
+			input:    InsertChar{Char: rune(0)},
+			expected: "\x00",
+		},
+		{
+			name:     "Single character",
+			input:    InsertChar{Char: 'a'},
+			expected: "a",
+		},
+		{
+			name:     "Special character",
+			input:    InsertChar{Char: '!'},
+			expected: "!",
+		},
+		{
+			name:     "Number character",
+			input:    InsertChar{Char: '1'},
+			expected: "1",
+		},
+		{
+			name:     "Unicode character",
+			input:    InsertChar{Char: '愛'},
+			expected: "愛",
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := tc.input.String()
+			if got != tc.expected {
+				t.Errorf("got %q, want %q", got, tc.expected)
+			}
+		})
+	}
+}
