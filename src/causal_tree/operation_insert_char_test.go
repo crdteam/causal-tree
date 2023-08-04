27196b0f63be175f63fa6f8f2947cf3d91bb8f52
ArturAssisComp <arturassiscomp@gmail.com>
2023-08-04 19:58:24 -0300

    Implement some unit tests for the insert char operation

--- src/causal_tree/operation_insert_char_test.go
+++ src/causal_tree/operation_insert_char_test.go
@@ -1,6 +1,10 @@
 package causal_tree
 
-import "testing"
+import (
+	"testing"
+
+	atm "github.com/crdteam/causal-tree/src/atom"
+)
 
 func Test_AtomPriority(t *testing.T) {
 	t.Run("Check if the atom priority returned is correct", func(t *testing.T) {
@@ -54,4 +58,63 @@ func Test_String(t *testing.T) {
 			}
 		})
 	}
+}
+
+func Test_ValidateChild(t *testing.T) {
+	testCases := []struct {
+		name   string
+		input  atm.AtomValue
+		gotNil bool
+	}{
+		{
+			name:   "InsertChar",
+			input:  InsertChar{Char: 'a'},
+			gotNil: true,
+		},
+		{
+			name:   "Delete",
+			input:  Delete{},
+			gotNil: true,
+		},
+		{
+			name:   "Other",
+			input:  InsertStr{},
+			gotNil: false,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			var v InsertChar
+			got := v.ValidateChild(tc.input)
+			if tc.gotNil {
+				if got != nil {
+					t.Errorf("got %q, want nil", got)
+				}
+			} else {
+				if got == nil {
+					t.Errorf("got nil, want %q", tc.input)
+				}
+			}
+		})
+	}
+}
+
+func Test_InsertChar(t *testing.T) {
+	t.Run("Insert a char", func(t *testing.T) {
+		// Create a causal tree
+		tree := NewCausalTree()
+
+		// Insert a char
+		err := tree.InsertChar('a')
+		if err != nil {
+			t.Errorf("got %q, want nil", err)
+		}
+
+		// Check if the value was inserted
+		got := tree.ToString()
+		expected := "a"
+		if got != expected {
+			t.Errorf("got %q, want %q", got, expected)
+		}
+	})
 }
